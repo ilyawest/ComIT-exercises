8b6afa54852dbe73f6f1380f7638ec4dfe4df229
Ilya West <Delta@Ilyas-MacBook-Pro-2.local>
2020-09-30 21:00:14 -0500

    Ex26

---
+++ EX26/index.js
@@ -0,0 +1,48 @@
+ 
+
+function isPrime(j) {
+    
+
+   
+for (let i = 1; i<j ; i++) 
+
+if (j%i==0) return j; 
+    
+}
+
+
+
+
+/*
+
+function sum(a, b) 
+{
+    return a + b;
+  }
+  
+  let result = sum(1, 2);
+  alert( result ); // 3
+
+
+function showMessage (message, type) {
+
+
+    if (type==i) {
+        console.log (message)
+    }
+
+}
+
+
+
+
+for (i = 1; i<100 ; i++) 
+
+do {
+   
+   i++
+
+  } while (i%2 !==0); 
+
+
+
---
+++ EX26/index2.js
@@ -0,0 +1,17 @@
+/*function showUser (message, type)
+{
+    if (type= "Name") 
+ {
+        console.log (message)
+  }
+        alert (message)
+}
+showUser ("Ilya", name)*/
+
+function logUser ()
+{  
+    console.log ("Ilya, Toronto");
+  
+}
+    logUser();
+
