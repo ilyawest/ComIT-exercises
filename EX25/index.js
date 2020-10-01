e77a9c03f5599458fe4c2bb194994fb6b45a8c24
Ilya West <Delta@Ilyas-MacBook-Pro-2.local>
2020-09-30 21:05:13 -0500

    Ex 25

---
+++ EX25/index.js
@@ -0,0 +1,58 @@
+
+
+
+let a = 1;
+
+switch (a) {
+  case 4:
+    alert('Right!');
+    break;
+
+  case 3: // (*) grouped two cases
+  case 5:
+    alert('Wrong!');
+    alert("Why don't you take a math class?");
+    break;
+
+  default:
+    alert('The result is strange. Really.');
+}
+
+
+
+
+
+
+
+
+
+let a = "1";
+let b = 0;
+
+switch (+a) {
+  case b + 1:
+}
+if (a==1) {
+    alert ("Today is Monday")
+    break;
+}
+if (a==2) {
+    alert ("Today is Tuesday")
+    break;
+}
+if (a==3) {
+    console.log ("Today is Wednesday")
+    break;
+}
+if (a==4) {
+    console.log ("Today is Tursday")
+    break;
+}
+if (a==5) {
+    console.log ("Today is Friday")
+    break;
+}
+
+else if    (b>7) 
+
+alert ('Error: Number not between 1-7')
